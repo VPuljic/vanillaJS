5bcf630aabd2b2851f1725f7387c57e5dc0dd06a
VP <puljic1979@gmail.com>
2021-06-22 20:39:18 +0200

    working on carousel

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 "use strict";
 
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
 fetch("../public/data.json")
   .then((response) => response.json())
   .then((data) => {
-    let output = "<h2>Kittens</h2>";
+    let output = "";
     data.forEach((kitten) => {
       output += `
       <div class="card">
@@ -11,7 +23,7 @@ fetch("../public/data.json")
       <h4><b>Name: ${kitten.name}</b></h4>
       <p>Age: ${kitten.age}</p>
       <p>Color: ${kitten.color}</p>
-      <p><button>Take ME</button></p>
+      <p><button>Take me</button></p>
       </div>
       `;
     });
