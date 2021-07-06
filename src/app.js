58a559ab659bb5ba16af99f23f1fccd60f4e7f77
VP <puljic1979@gmail.com>
2021-07-06 21:33:29 +0200

    Added buttons on carousel

--- src/app.js
+++ src/app.js
@@ -10,6 +10,16 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for (let i = 0; i < marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
+function changeDirection() {
+  let marqueeScroleFirst = getComputedStyle(root).getPropertyValue(
+    "--marquee-scroling-first"
+  );
+  let marqueeScroleSecond = getComputedStyle(root).getPropertyValue(
+    "--marquee-scroling-second"
+  );
+  root.style.setProperty("--marquee-scroling-first", marqueeScroleSecond);
+  root.style.setProperty("--marquee-scroling-second", marqueeScroleFirst);
+}
 
 // List of kitten "cards"
 const showMore = document.getElementById("showMore");
